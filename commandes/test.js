c33db371240967fcb4ba2481d1e801bb13f98cca
LOA <118371674+Gbevon@users.noreply.github.com>
2024-02-27 00:16:27 +0000

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -1,27 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "test", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle *Zokou* \n\n ' + 'je suis un bot Whatsapp Multi-appareil ';
-    let d = ' developpé par *Djalega++*';
-    let varmess = z + d;
-    var img = 'https://wallpapercave.com/uwp/uwp3842939.jpeg';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
-});
-console.log("mon test");
-/*module.exports.commande = () => {
-  var nomCom = ["test","t"]
-  var reaction="☺️"
-  return { nomCom, execute,reaction }
-};
+const { zokou } = require('../framework/zokou');
 
-async function  execute  (origineMessage,zok) {
-  console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *Djalega++*'
-      let varmess=z+d
-      var img='https://wallpapercave.com/uwp/uwp3842939.jpeg'
-await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
-}  */ 
+zokou(
+    {
+        nomCom: 'pray',
+        categorie: 'loryse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = ' https://telegra.ph/file/37b10f9e2a9c9a0349e85.jpg';
+            const msg = `Salutation✨ a tous!!!
+Ici Leroi du chaos absolu Le_*ꪶ࿋྄ིᤢꫂ๖🔯ℛɨᎮ昇ⅅᎯℛᏦ ᏦℐℕᎶ⛓️
+Attristé par la démance de ce monde nous sommes dans l'obligation de lancé une new vague de purification dans les ténèbres du ꪶ࿋྄ིᤢꫂ๖ۣۜB₳Č₭ M࿋❂྄྄ིᤢꫂ₦🌑 plus destructrice ke le first. En tant que la 1er section du 𓆩†★ ⛩E_M_P_I_R_E🎴OF🎴TOMAN ⛩ 𓆩†★ la futur plus grand organisation de purge je vous donne la chance d'être les Hôtes de cette grande catastrophe et non les victimes . trainer en bas ou devenir une légende ?
+ꪶ࿋྄ིᤢꫂ๖ۣۜB₳Č₭ M࿋❂྄྄ིᤢꫂ₦🌑 recrute 👉https://chat.whatsapp.com/GhP9QNwUIwg9kLnlAEgA2V`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
